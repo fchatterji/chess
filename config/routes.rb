610cb0114594b9df8136bbc26e1b35cd15b45128
Florent Chatterji <florent.chatterji@gmail.com>
2023-02-14 17:38:44 -0300

    add authentication with auth0

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  get "dashboard", to: "dashboard#show"
+
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
 end
